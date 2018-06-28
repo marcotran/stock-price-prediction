cdf6f3f2e9cba96e76ff7a1c3c684fee273b5bd1
pskrunner14 <pskrunner14@gmail.com>
2018-06-28 22:59:00 +0530

    ref localhost errors

--- app.py
+++ app.py
@@ -76,6 +76,5 @@ def getStockData(method):
     return jsonify(data)
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
