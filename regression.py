43bb45c82be58562cb290c6e8b08d3201995f406
pskrunner14 <pskrunner14@gmail.com>
2018-07-11 02:05:48 +0530

    adds LSTM model

--- regression.py
+++ regression.py
@@ -1,5 +1,8 @@
 from sklearn import cross_validation, svm
 import sklearn.linear_model as lin
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
 
 def DatasetSplit(X, y):
     return cross_validation.train_test_split(X, y, test_size=0.3)
@@ -41,3 +44,25 @@ def ARIMARegression(X, y, X_data):
 
 def LSTMRegression(X, y, X_data):
     X_train, X_test, y_train, y_test = DatasetSplit(X, y)
+    
+    model = Sequential()
+    model.add(LSTM(
+        input_shape=(None, input_dim),
+        units=output_dim,
+        return_sequences=return_sequences))
+    model.add(Dropout(0.2))
+    model.add(LSTM(
+        128, 
+        return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+    model.add(Activation('linear'))
+
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics = ["accuracy"])
+    model.fit(X=X_train, y=y_train, epochs=20, batch_size=50, verbose=1)
+
+    preds = model.evaluate(X=X_test, y=y_test, verbose  = 1)
+
+    print()
+    print ("Loss = " + str(preds[0]))
+    print ("Test Accuracy = " + str(preds[1]))
