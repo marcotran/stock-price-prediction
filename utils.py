9942ba0470af6ba9c0d5f5f2aa12e7ff37ec1e85
marcotran <tranthanhha12.1996@gmail.com>
2018-10-09 10:20:41 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,59 +1,26 @@
-import math
-import random
 import numpy as np
+import pandas as pd
 from flask import jsonify
 from sklearn import preprocessing
-from sklearn import cross_validation
+import json
 
-def DatasetSplit(X, y):
-    return cross_validation.train_test_split(X, y, test_size=0.1)
+import warnings
+warnings.filterwarnings('ignore')
 
-def formatForModel(dataArray):
-    dataArray = dataArray[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-    dataArray['HL_PCT'] = (dataArray['Adj. High'] - dataArray['Adj. Close']) / dataArray['Adj. Close'] * 100.0
-    dataArray['PCT_change'] = (dataArray['Adj. Close'] - dataArray['Adj. Open']) / dataArray['Adj. Open'] * 100.0
-    dataArray = dataArray[['Adj. Close', 'HL_PCT', 'PCT_change','Adj. Volume']]
-    dataArray.fillna(-99999, inplace=True)
-    return dataArray
-
-def processData(apiData):
-    dataLength = 251
-    allDataLength = len(apiData)
-    firstDataElem = math.floor(random.random()*(allDataLength-dataLength))
-    mlData = apiData[0:firstDataElem+dataLength]
-
-    mlData = formatForModel(mlData)
-
-    forecast_col = 'Adj. Close'
-    forecast_out = int(math.ceil(0.12*dataLength))
-
-    mlData['label'] = mlData[forecast_col].shift(-forecast_out)
-    mlData.dropna(inplace=True)
-
-    # get all the columns except the ouput label
-    X = np.array(mlData.drop(['label'], 1))
-    # preprocess data
+def process_data(stock_data):
+    pred_data = 365
+    forecast_column = 'Adj Close'
+    dates = np.array(stock_data['Date'].values)
+    X = np.array(stock_data[forecast_column].values)[1:]
+    y = np.array(stock_data[forecast_column].shift(1))[1:]
     X = preprocessing.scale(X)
-    # separate some of the data to be predicted
 
-    X_arima = mlData[['label']]
+    return dates[-pred_data:], X[:-pred_data].reshape(-1, 1), X[-pred_data:].reshape(-1, 1), y[:-pred_data], y[-pred_data:]
 
-    X_arima_predict = X_arima[-dataLength:]
-    X_arima_model = X_arima[:-dataLength]
-
-    X_predict = X[-dataLength:]
-    # separate the rest of the data for the model to be trained on
-    X_model = X[:-dataLength]
-    # get the output labels of the data to be predicted
-    y_actual = mlData[-dataLength:]
-    y_actual = y_actual[['Adj. Close']]
-    mlData = mlData[:-dataLength]
-    y_model = np.array(mlData['label'])
-
-    return X_model, y_model, X_predict, y_actual, X_arima_model, X_arima_predict
-
-def packageData(data, prediction, accuracy):
-    data = data.rename(columns={'Adj. Close':'actual'})
-    data['prediction'] = prediction[:]
-    data = data.to_json(orient='table')
+def package_data(dates, y_predict, prediction, result):
+    data = {'date': dates[:], 'actual': y_predict[:], 'prediction': prediction[:]}
+    df = pd.DataFrame(data=data)
+    data = df.to_json(orient='table')
+    data = json.loads(data)
+    data['result'] = result
     return jsonify(data)
